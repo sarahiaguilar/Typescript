39d9e00c1cdb772b1c17c77d276720dedc21bbb0
Sarahí Aguilar <sarahiaguilargonzalez@gmail.com>
2018-07-25 17:06:31 -0400

    Let variables

---
+++ variables-let.js
@@ -0,0 +1,7 @@
+var nombre = "Juan";
+if (nombre == "Juan") {
+    // LET se utiliza ara hacer en bloques de código dentro de una instrucción ya sea una función o una condición
+    var nombre_1 = "Pedro";
+    console.log("Dentro de la condición es igual a " + nombre_1); // Pedro
+}
+console.log("Dentro de la condición es igual a " + nombre); // Juan
---
+++ variables-let.ts
@@ -0,0 +1,8 @@
+var nombre = "Juan";
+
+if (nombre == "Juan") {
+	// LET se utiliza ara hacer en bloques de código dentro de una instrucción ya sea una función o una condición
+	let nombre = "Pedro";
+	console.log("Dentro de la condición es igual a " + nombre); // Pedro
+}
+console.log("Dentro de la condición es igual a " + nombre); // Juan
